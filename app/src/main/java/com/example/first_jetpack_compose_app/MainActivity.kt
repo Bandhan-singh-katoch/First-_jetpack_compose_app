ea593a33f757db27a8b3a0cf43f5a5989d422aeb
Bandhan singh <parbhatbandhan@gmail.com>
2021-08-24 19:39:30 +0530

    added background using Surface() and implemented padding by using Modifier.padding

--- app/src/main/java/com/example/first_jetpack_compose_app/MainActivity.kt
+++ app/src/main/java/com/example/first_jetpack_compose_app/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.first_jetpack_compose_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.first_jetpack_compose_app.ui.theme.First_Jetpack_compose_appTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +30,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = Color.Green) {
+        Text(text = "Hello $name!",modifier = Modifier.padding(22.dp))
+    }
 }
 
 @Preview(showBackground = true)
