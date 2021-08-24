c6fa587f6cb67e122f3d5d3efd862dd94e935e8e
Bandhan singh <parbhatbandhan@gmail.com>
2021-08-24 23:27:05 +0530

    mistake improved

--- app/src/main/java/com/example/first_jetpack_compose_app/MainActivity.kt
+++ app/src/main/java/com/example/first_jetpack_compose_app/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                    Greeting("Android")
+                    MyApp()
         }
     }
 }
